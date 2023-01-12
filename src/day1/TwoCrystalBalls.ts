90fd7bae769fc42ed570268e7d4b26f338f185e0
erietz <ewrietz@gmail.com>
2023-01-12 11:03:30 -0600

    two balls initial dumb approach

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,26 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+  const calcStory = (i: number) => i * Math.floor(Math.sqrt(breaks.length));
+
+  let broke = false;
+  let story;
+  let i = 0;
+  do {
+    story = calcStory(i);
+    if (breaks[story]) {
+      broke = true;
+      break;
+    }
+    i++;
+  } while (story < breaks.length)
+
+  if (broke) {
+    for (let j = calcStory(i - 1); j < calcStory(i); j++) {
+      if (breaks[j]) {
+        return j;
+      }
+    }
+  }
+
+  return -1;
 }
