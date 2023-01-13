d9f9bb29b1e5e97eb65c1296aa0992d6799f7cd8
erietz <ewrietz@gmail.com>
2023-01-13 12:11:39 -0600

    stack done

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,36 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private _head?: ListNode<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node: ListNode<T> = { value: item };
 
-}
+        if (!this._head) {
+            this._head = node;
+        } else {
+            node.next = this._head;
+            this._head = node;
+        }
+
+        this.length++;
+    }
+
     pop(): T | undefined {
+        if (!this._head) {
+            return undefined;
+        }
 
-}
+        const node = this._head;
+        this._head = this._head.next;
+        this.length--;
+        return node.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this._head?.value;
+    }
 }
