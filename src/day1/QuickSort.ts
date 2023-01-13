ab50bde0e6eb9ee8980aff78eb6b3b4c09ab42bc
erietz <ewrietz@gmail.com>
2023-01-13 15:29:40 -0600

    quicksort done

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,30 @@
-export default function quick_sort(arr: number[]): void {
+function quickSort(arr: number[], lo: number, hi: number): void  {
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, lo, hi);
+    quickSort(arr, lo, pivotIdx - 1);
+    quickSort(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number  {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    return quickSort(arr, 0, arr.length - 1);
 }
